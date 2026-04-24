0c620856d60c4d93dea14461653c8eed8f381b2f
Ridhima0831 <ridhimabhardwaj2031@gmail.com>
2026-04-24 10:50:10 +0530

    Create App component for Charity Donation System

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", padding: "40px" }}>
+      <h1>🎗️ Charity Donation System</h1>
+      <p>App is running successfully 🚀</p>
+
+      <div style={{ marginTop: "20px" }}>
+        <h3>Pages:</h3>
+        <p>✔ Login</p>
+        <p>✔ Signup</p>
+        <p>✔ Dashboard</p>
+        <p>✔ Donate</p>
+        <p>✔ Scanner</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
